249600730ab71089600e5b08e4821fed06df69a8
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2017-02-21 13:53:23 +0100

    implement POST to add an entry + magically handle _id vs id

--- api/db.js
+++ api/db.js
@@ -20,6 +20,24 @@ const get_client = () => (
         : get_client_raw()
 );
 
+
+const v_export_id = (v) => {
+    if (v) {
+        if (!("id" in v)) v.id = v._id;
+        delete v._id;
+    }
+    return v;
+}
+
+const toObjectID = (id) => (
+    id === null || mongodb.ObjectID.isValid(id) ? new mongodb.ObjectID(id) : null 
+);
+
+exports.v_id = (id) => { 
+    let _id = toObjectID(id);
+    return _id ? { _id } : { id };
+};
+
 exports.collection = (db_name, collection_name) => (
     get_client().then(client => {
         if (!collections_cache[db_name]) collections_cache[db_name] = {};
@@ -35,17 +53,17 @@ exports.collection = (db_name, collection_name) => (
 );
 
 exports.get = (collection, criteria) => (
-    collection.find(criteria).limit(1).next()
+    collection.find(criteria).limit(1).next().then(v_export_id)
 );
 
 exports.find = (collection, criteria) => (
-    collection.find(criteria).toArray()
+    collection.find(criteria).toArray().then(l => l.map(v_export_id))
 );
 
 exports.delete = (collection, criteria) => (
     collection.deleteOne(criteria).then(result => {
         //console.log("delete", criteria, result);
-        return result.deletedCount ? true : Promise.reject("not found");
+        return result.deletedCount ? { ok: 1 } : Promise.reject("not found");
     })
 );
 
@@ -53,5 +71,7 @@ exports.save = (collection, criteria, v) => {
     Object.assign(v, criteria);
     v.modifyTimestamp = new Date();
     console.log("saving in DB:", v);
-    return collection.updateOne(criteria, v, {upsert: true});
+    return collection.updateOne(criteria, v, {upsert: true}).then(_ => (
+        { ok: 1, id: "id" in v ? v.id : v._id }
+    ));
 };
--- api/routes.js
+++ api/routes.js
@@ -31,13 +31,15 @@ const collection = (req) => (
     db.collection(req.params.db, req.params.collection)
 );
 
+const v_id = db.v_id;
+
 const v_addUid = (req, v) => {
     v.uid = req.user.id;
     return v;
 };
 
 const v_removeUid = (v) => {
-    if (v) { delete v.uid; delete v._id; }
+    if (v) { delete v.uid; }
     return v;
 };
 
@@ -54,26 +56,32 @@ const getAll = (req) => collection(req).then(collection => (
     db.find(collection, {}).then(array2map)
 ));
 const get = (req) => collection(req).then(collection => (
-    db.get(collection, { id: req.params.id })
+    db.get(collection, v_id(req.params.id))
 ));
 const delete_ = (req) => collection(req).then(collection => (
-    db.delete(collection, { id: req.params.id })
+    db.delete(collection, v_id(req.params.id))
 ));
 const put = (req) => collection(req).then(collection => (
-    db.save(collection, { id: req.params.id }, req.body)
+    db.save(collection, v_id(req.params.id), req.body)
+));
+const add = (req) => collection(req).then(collection => (
+    db.save(collection, v_id(null), req.body)
 ));
 
 const getAllWithUid = (req) => collection(req).then(collection => (
     db.find(collection, v_addUid(req, {})).then(l => array2map(l.map(v_removeUid)))
 ));
 const getWithUid = (req) => collection(req).then(collection => (
-    db.get(collection, v_addUid(req, { id: req.params.id })).then(v_removeUid)
+    db.get(collection, v_addUid(req, v_id(req.params.id))).then(v_removeUid)
 ));
 const deleteWithUid = (req) => collection(req).then(collection => (
-    db.delete(collection, v_addUid(req, { id: req.params.id }))
+    db.delete(collection, v_addUid(req, v_id(req.params.id)))
 ));
 const putWithUid = (req) => collection(req).then(collection => (
-    db.save(collection, v_addUid(req, { id: req.params.id }), req.body)
+    db.save(collection, v_addUid(req, v_id(req.params.id)), req.body)
+));
+const addWithUid = (req) => collection(req).then(collection => (
+    db.save(collection, v_addUid(req, v_id(null)), req.body)
 ));
 
 const with_acl = (f) => (req, res) => (
@@ -94,10 +102,12 @@ router.get("/.login", login);
 router.get("/:db/:collection/\\$user/:id", with_acl(getWithUid));
 router.put('/:db/:collection/\\$user/:id', with_acl(putWithUid));
 router.delete('/:db/:collection/\\$user/:id', with_acl(deleteWithUid));
+router.post('/:db/:collection/\\$user', with_acl(addWithUid));
 router.get('/:db/:collection/\\$user', with_acl(getAllWithUid));
 router.get("/:db/:collection/:id", with_acl(get));
 router.put("/:db/:collection/:id", with_acl(put));
 router.delete("/:db/:collection/:id", with_acl(delete_));
+router.post("/:db/:collection", with_acl(add));
 router.get("/:db/:collection", with_acl(getAll));
 
 
